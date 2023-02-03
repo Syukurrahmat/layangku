d9b75476b8cbaec23ce8bc64e2c11fe1568c80c9
Syukurrahmat <syukurtullah@gmail.com>
2023-02-03 23:54:35 +0700

    fix and update server

--- server/app.js
+++ server/app.js
@@ -2,20 +2,18 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const handler = require('serve-handler');
-const api = require('./router/api.js');
+const api = require('./router/api.js')
 const path = require('path')
-const cors = require('cors')
 const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors({
-    origin: '*'
-}));
-app.use('/api', api);
+app.use(express.static('public'))
 
-app.get('*', (req, res) => handler(req, res, { public: path.join('public') }))
+
+app.use('/api', api)
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
 
 app.listen(process.env.PORT || 5000, () => console.log('server aktif'))
